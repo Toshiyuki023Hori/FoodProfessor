fdd9a6c26535916ff69e5f1be704baed0ef9185f
akira-iguchi <akituri931@icloud.com>
2021-06-21 20:28:59 +0900

    ESLintとPrettier設定

--- front/react-app/src/App.tsx
+++ front/react-app/src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from 'react'
 
 function App() {
   return (
     <div className="App">
-      <p className="text-3xl text-red-600">aaaaaaaa</p>
+      <p className="text-5xl text-red-600">aaaaaaaa</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
