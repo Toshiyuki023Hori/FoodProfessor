10bc037a6b127ce07ba51472247b7a914634cbd5
akira-iguchi <akituri931@icloud.com>
2021-06-29 17:35:18 +0900

    purposeテーブル編集

--- api/app/models/purpose.rb
+++ api/app/models/purpose.rb
@@ -6,6 +6,6 @@ class Purpose < ApplicationRecord
   validates :recipe_id, presence: true
   validates :purpose_name, { presence: true, length: { maximum: 50 } }
   validates :purpose_content, length: { maximum: 200 }
-  validates :purpose_for_people, numericality: { greater_than: 0, only_integer: true }
-  validates :purpose_time, numericality: { greater_than: 0, only_integer: true }
+  validates :purpose_for_people, numericality: { greater_than: 0, only_integer: true }, allow_blank: true
+  validates :purpose_time, numericality: { greater_than: 0, only_integer: true }, allow_blank: true
 end
