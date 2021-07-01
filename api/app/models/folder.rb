590f78dac64fd4a6e499d67fef351798656ace02
akira-iguchi <akituri931@icloud.com>
2021-07-01 20:46:00 +0900

    dependent: :destroy追加

--- api/app/models/folder.rb
+++ api/app/models/folder.rb
@@ -1,6 +1,6 @@
 class Folder < ApplicationRecord
   belongs_to :user
-  has_many :recipes
+  has_many :recipes, dependent: :destroy
 
   validates :user_id, presence: true
   validates :folder_name, { presence: true, length: { maximum: 50 } }
